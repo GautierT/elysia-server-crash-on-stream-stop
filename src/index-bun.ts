cc4d204a9e051b3be8408a5bb7208bad58b895b6
Gautier <6089653+GautierT@users.noreply.github.com>
2024-10-16 17:10:11 +0200

    add bun exemple

--- src/index-bun.ts
+++ src/index-bun.ts
@@ -1,19 +1,13 @@
 import { createAnthropic } from "@ai-sdk/anthropic";
-import { serve } from "@hono/node-server";
-import { StreamData, streamText } from "ai";
-import { Elysia } from "elysia";
-import { Hono } from "hono";
-import { stream } from "hono/streaming";
+import { streamText } from "ai";
 
 export const anthropic = createAnthropic({
 	//apiKey: "YOUR_API_KEY",
 });
 
-const hono = new Hono()
-	.post("/", async (c) => {
-		console.log("Start POST /");
-		console.log("method : ", c.req.method);
-
+Bun.serve({
+	port: 3000,
+	fetch: async (req) => {
 		const res = await streamText({
 			model: anthropic("claude-3-5-sonnet-20240620", { cacheControl: true }),
 			temperature: 0,
@@ -27,64 +21,7 @@ const hono = new Hono()
 		});
 
 		return res.toDataStreamResponse();
-	})
-	// https://sdk.vercel.ai/examples/api-servers/hono
-	.post("/2", async (c) =>
-		stream(c, async (stream) => {
-			const result = await streamText({
-				model: anthropic("claude-3-5-sonnet-20240620", { cacheControl: true }),
-				prompt: "write a very long poem (more than 1000 words) about life",
-			});
 
-			// Mark the response as a v1 data stream:
-			c.header("X-Vercel-AI-Data-Stream", "v1");
-			c.header("Content-Type", "text/plain; charset=utf-8");
-
-			await stream.pipe(result.toDataStream());
-		}),
-	)
-	.post("/3", async (c) =>
-		stream(c, async (stream) => {
-			// use stream data (optional):
-			const data = new StreamData();
-			data.append("initialized call");
-
-			const result = await streamText({
-				model: anthropic("claude-3-5-sonnet-20240620", { cacheControl: true }),
-				prompt: "write a very long poem (more than 1000 words) about life",
-			});
-
-			// Mark the response as a v1 data stream:
-			c.header("X-Vercel-AI-Data-Stream", "v1");
-			c.header("Content-Type", "text/plain; charset=utf-8");
-
-			await stream.pipe(result.toDataStream({ data }));
-		}),
-	);
-
-console.log(`Hono is running`);
-
-export default hono;
-
-/* const app = new Elysia()
-	.post("/", async ({ request }) => {
-		const res = await streamText({
-			model: anthropic("claude-3-5-sonnet-20240620", { cacheControl: true }),
-			temperature: 0,
-			messages: [
-				{
-					role: "user",
-					content: "write a very long poem (more than 1000 words) about life",
-				},
-			],
-			//abortSignal: request.signal,
-		});
-
-		return res.toDataStreamResponse();
-	})
-	.listen(3000);
-
-console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
- */
+		//return new Response("Hello World from bun");
+	},
+});
